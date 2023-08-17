6b7e8d9263cc387bb9b398db7d5d6fb404357363
Antonov333 <antonovsergei333@yahoo.com>
2023-08-17 10:32:49 +0300

    Avatar entity class created

--- src/main/java/pro/sky/course3lesson5scratch/model/Avatar.java
+++ src/main/java/pro/sky/course3lesson5scratch/model/Avatar.java
@@ -2,8 +2,82 @@ package pro.sky.course3lesson5scratch.model;
 
 import org.springframework.beans.factory.annotation.Value;
 
+import javax.persistence.*;
+
+@Entity
 public class Avatar {
 
     @Value("${path.to.avatars.folder}")
     private String avatarsDir;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String filePath;
+    private long fileSize;
+    private String mediaType;
+    private byte[] data;
+
+    @OneToOne
+    private Student student;
+
+    public Avatar() {
+    }
+
+    public String getAvatarsDir() {
+        return avatarsDir;
+    }
+
+    public void setAvatarsDir(String avatarsDir) {
+        this.avatarsDir = avatarsDir;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getFilePath() {
+        return filePath;
+    }
+
+    public void setFilePath(String filePath) {
+        this.filePath = filePath;
+    }
+
+    public long getFileSize() {
+        return fileSize;
+    }
+
+    public void setFileSize(long fileSize) {
+        this.fileSize = fileSize;
+    }
+
+    public String getMediaType() {
+        return mediaType;
+    }
+
+    public void setMediaType(String mediaType) {
+        this.mediaType = mediaType;
+    }
+
+    public byte[] getData() {
+        return data;
+    }
+
+    public void setData(byte[] data) {
+        this.data = data;
+    }
+
+    public Student getStudent() {
+        return student;
+    }
+
+    public void setStudent(Student student) {
+        this.student = student;
+    }
 }
---
+++ src/main/java/pro/sky/course3lesson5scratch/service/StudentService.java
@@ -0,0 +1,80 @@
+package pro.sky.course3lesson5scratch.service;
+
+//import org.springframework.http.HttpStatusCode;
+
+import org.springframework.data.domain.Example;
+import org.springframework.stereotype.Service;
+import pro.sky.course3lesson5scratch.exception.StudentAlreadyExistsException;
+import pro.sky.course3lesson5scratch.exception.StudentNotFoundException;
+import pro.sky.course3lesson5scratch.model.Faculty;
+import pro.sky.course3lesson5scratch.model.Student;
+import pro.sky.course3lesson5scratch.repository.StudentRepository;
+
+import java.util.List;
+
+@Service
+public class StudentService {
+
+    private final StudentRepository studentRepository;
+
+    public StudentService(StudentRepository studentRepository) {
+        this.studentRepository = studentRepository;
+    }
+
+    public Student createStudent(Student student) {
+        Example<Student> studentExample = Example.of(student);
+        if (studentRepository.exists(studentExample)) {
+            throw new StudentAlreadyExistsException();
+        }
+        return studentRepository.save(student);
+    }
+
+    public Student updateStudent(long id, String newName, int newAge) {
+        Student existingStudent = studentRepository.findById(id).orElseThrow(StudentNotFoundException::new);
+        if (newName != null) {
+            existingStudent.setName(newName);
+        }
+        if (newAge > 15) {
+            existingStudent.setAge(newAge);
+        }
+        return studentRepository.save(existingStudent);
+    }
+
+    public List<Student> getStudents() {
+        return studentRepository.findAll();
+    }
+
+    public Student sendDown(Student student) {
+        studentRepository.delete(student);
+        return student;
+    }
+
+    public List<Student> selectedByAge(int age) {
+        return studentRepository.findAllByAge(age);
+    }
+
+    public List<Student> selectedByAgeBetween(int a, int b) {
+
+        if (a == b) {
+            return studentRepository.findAllByAge(a);
+        }
+
+        int min, max;
+        if (a < b) {
+            min = a;
+            max = b;
+        } else {
+            min = b;
+            max = a;
+        }
+
+        return studentRepository.findByAgeBetween(min, max);
+
+    }
+
+    public Faculty getStudentFaculty(long id) {
+        Student student = studentRepository.findById(id).orElseThrow(StudentNotFoundException::new);
+        return student.getFaculty();
+    }
+
+}
