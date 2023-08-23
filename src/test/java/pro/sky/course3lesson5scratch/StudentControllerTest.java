ed6895caedee57f8f95576bf68084b18fb277943
Antonov333 <antonovsergei333@yahoo.com>
2023-08-23 13:28:12 +0300

    Composed method viewStudentsTest() to test "http://localhost/student" endpoint

--- src/test/java/pro/sky/course3lesson5scratch/StudentControllerTest.java
+++ src/test/java/pro/sky/course3lesson5scratch/StudentControllerTest.java
@@ -7,22 +7,29 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.boot.test.web.server.LocalServerPort;
 import pro.sky.course3lesson5scratch.controller.StudentController;
+import pro.sky.course3lesson5scratch.model.Student;
+
+import java.util.List;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 public class StudentControllerTest {
 
+    @Autowired
+    StudentController studentController; // instance of controller class under test
+    @Autowired
+    TestRestTemplate testRestTemplate; // instance of testing tool class to test REST requests
     @LocalServerPort
     private int port;
 
-    @Autowired
-    StudentController studentController; // instance of controller class under test
-
-    @Autowired
-    TestRestTemplate testRestTemplate; // instance of testing tool class to test REST requests
-
     @Test
     void checkContextLoads() throws Exception {
-        System.out.println("port = " + port);
         Assertions.assertThat(studentController).isNotNull();
+    }
+
+    @Test
+    void viewStudentsTest() {
+        Assertions.assertThat(
+                        this.testRestTemplate.getForObject("http://localhost:" + port + "/student", Object.class))
+                .isNotNull();
     }
 }
