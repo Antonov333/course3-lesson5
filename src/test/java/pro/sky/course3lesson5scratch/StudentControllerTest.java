87cc0261c2c2a1daa55511e519bbb43d254e99c1
Antonov333 <antonovsergei333@yahoo.com>
2023-08-23 13:02:18 +0300

    Composed test to make sure StudentController Context loads

--- src/test/java/pro/sky/course3lesson5scratch/StudentControllerTest.java
+++ src/test/java/pro/sky/course3lesson5scratch/StudentControllerTest.java
@@ -1,4 +1,28 @@
 package pro.sky.course3lesson5scratch;
 
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.boot.test.web.server.LocalServerPort;
+import pro.sky.course3lesson5scratch.controller.StudentController;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 public class StudentControllerTest {
+
+    @LocalServerPort
+    private int port;
+
+    @Autowired
+    StudentController studentController; // instance of controller class under test
+
+    @Autowired
+    TestRestTemplate testRestTemplate; // instance of testing tool class to test REST requests
+
+    @Test
+    void checkContextLoads() throws Exception {
+        System.out.println("port = " + port);
+        Assertions.assertThat(studentController).isNotNull();
+    }
 }
