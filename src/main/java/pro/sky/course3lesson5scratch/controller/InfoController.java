8d1e60c395fcca0a88e6561cd78089005f084519
Antonov333 <antonovsergei333@yahoo.com>
2023-09-10 17:14:53 +0300

    Composed InfoController and profile files

--- src/main/java/pro/sky/course3lesson5scratch/controller/InfoController.java
+++ src/main/java/pro/sky/course3lesson5scratch/controller/InfoController.java
@@ -1,16 +1,19 @@
 package pro.sky.course3lesson5scratch.controller;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/info")
 public class InfoController {
 
-    @GetMapping
+    @Value("${server.port}")
+    private int port;
+
+    @GetMapping(value = "info")
     int getPort() {
-        return -1;
+        return port;
     }
 
 }
